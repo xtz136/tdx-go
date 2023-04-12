03fd9c3fec22457b118a0164ca87efcb5694e73c
xtz <xutaozhe@126.com>
2023-04-12 12:08:37 +0800

    fix pareAmount

--- proto/v1/helper.go
+++ proto/v1/helper.go
@@ -10,52 +10,53 @@ import (
 
 func ParseVolume(ivol int) float64 {
 	logpoint := ivol >> (8 * 3)
-	hleax := (ivol >> (8 * 2)) & 0xff
-	lheax := (ivol >> 8) & 0xff
-	lleax := ivol & 0xff
+	hleax := (ivol >> (8 * 2)) & 0xff // [2]
+	lheax := (ivol >> 8) & 0xff       // [1]
+	lleax := ivol & 0xff              // [0]
 
 	dwEcx := logpoint*2 - 0x7f
 	dwEdx := logpoint*2 - 0x86
 	dwEsi := logpoint*2 - 0x8e
 	dwEax := logpoint*2 - 0x96
 
-	tmpEax := 0
+	var tmpEax int
 	if dwEcx < 0 {
 		tmpEax = -dwEcx
 	} else {
 		tmpEax = dwEcx
 	}
 
-	dblXmm6 := math.Pow(2.0, float64(tmpEax))
-	if dwEax < 0 {
-		dblXmm6 = 1.0 / dblXmm6
+	var dbl_xmm6 float64
+	dbl_xmm6 = math.Pow(2.0, float64(tmpEax))
+	if dwEcx < 0 {
+		dbl_xmm6 = 1.0 / dbl_xmm6
 	}
 
-	var dblXmm4 float64
+	var dbl_xmm4 float64
 	if hleax > 0x80 {
-		dwtmpeax := dwEdx + 1
-		tmpdblXmm3 := math.Pow(2.0, float64(dwtmpeax))
-		dblXmm0 := math.Pow(2.0, float64(dwEdx)) * 128.0
-		dblXmm0 += float64(hleax&0x7f) * tmpdblXmm3
-		dblXmm4 = dblXmm0
+		var dwtmpeax = float64(dwEdx + 1)
+		tmpdbl_xmm3 := math.Pow(2.0, dwtmpeax)
+		dbl_xmm0 := math.Pow(2.0, float64(dwEdx)) * 128.0
+		dbl_xmm0 += float64(hleax&0x7f) * tmpdbl_xmm3
+		dbl_xmm4 = dbl_xmm0
 	} else {
-		dblXmm0 := 0.0
+		var dbl_xmm0 float64
 		if dwEdx >= 0 {
-			dblXmm0 = math.Pow(2.0, float64(dwEdx)) * float64(hleax)
+			dbl_xmm0 = math.Pow(2.0, float64(dwEdx)) * float64(hleax)
 		} else {
-			dblXmm0 = (1 / math.Pow(2.0, float64(dwEdx))) * float64(hleax)
+			dbl_xmm0 = (1 / math.Pow(2.0, float64(dwEdx))) * float64(hleax)
 		}
-		dblXmm4 = dblXmm0
+		dbl_xmm4 = dbl_xmm0
 	}
 
-	dblXmm3 := math.Pow(2.0, float64(dwEsi)) * float64(lheax)
-	dblXmm1 := math.Pow(2.0, float64(dwEax)) * float64(lleax)
-	if hleax&0x80 > 0 {
-		dblXmm3 *= 2.0
-		dblXmm1 *= 2.0
+	dbl_xmm3 := math.Pow(2.0, float64(dwEsi)) * float64(lheax)
+	dbl_xmm1 := math.Pow(2.0, float64(dwEax)) * float64(lleax)
+	if hleax&0x80 != 0 {
+		dbl_xmm3 *= 2.0
+		dbl_xmm1 *= 2.0
 	}
 
-	return dblXmm6 + dblXmm4 + dblXmm3 + dblXmm1
+	return dbl_xmm6 + dbl_xmm4 + dbl_xmm3 + dbl_xmm1
 }
 
 type Struct struct{}
